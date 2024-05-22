08ef788d5112f53608e29198d56529902e329a93
RidloGhifary <ridloghfry@gmail.com>
2024-05-22 15:05:01 +0700

    chore(public): make a public static file

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ import mongoSanitize from "express-mongo-sanitize";
 import helmet from "helmet";
 import cors from "cors";
 
+// ? MOCK_DATA
+import mockDataUsers from "./mockData/users.json";
+import mockDataProducts from "./mockData/products.json";
+import mockDataOrders from "./mockData/orders.json";
+import mockDataReviews from "./mockData/reviews.json";
+
 const app: Application = express();
 const port: number = 5100;
 
@@ -14,16 +20,30 @@ const limiter = rateLimit({
   legacyHeaders: false,
 });
 
+const mockData = [
+  {
+    users: [...mockDataUsers],
+    products: [...mockDataProducts],
+    review: [...mockDataReviews],
+    orders: [...mockDataOrders],
+  },
+];
+
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(limiter);
 app.use(mongoSanitize());
+app.use(express.static("./public"));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
-});
+const startServer = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is running on PORT:${port}`);
+    });
+  } catch (err) {
+    console.log("🚀 ~ startServer ~ err:", err);
+  }
+};
 
-app.listen(port, () => {
-  console.log(`Server is running on PORT:${port}`);
-});
+startServer();
---
+++ src/public/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const cartButton = document.getElementById("cart-button");
+  cartButton.addEventListener("click", function () {
+    alert("Item added to cart!");
+  });
+});
