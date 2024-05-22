91eef0d5cfa43a6c814e485d6853f3f4aa3af138
RidloGhifary <ridloghfry@gmail.com>
2024-05-22 14:31:23 +0700

    chore(nodemon): restarting the server automatically

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
-const port: number = 5000;
+const port: number = 5100;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
